307746bcb76b238fcd16af5bc431e353a32ce26e
Abdullah Belli <belliabdullah@gmail.com>
2025-07-03 23:01:19 +0300

    kotlin

---
+++ CalismaYapisi/app/src/main/java/com/example/calismayapisi/DetayActivity.kt
@@ -0,0 +1,22 @@
+package com.example.calismayapisi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.calismayapisi.databinding.ActivityDetayBinding
+import com.example.calismayapisi.databinding.ActivityMainBinding
+
+class DetayActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetayBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val gelenMesaj = intent.getStringExtra("mesaj")
+
+        binding.textViewSonuc.text = gelenMesaj
+    }
+}
